e015655633eb1974cfede3d529b4580eebfa4bd5
Haioner <kelvynlee2010@gmail.com>
2023-06-14 03:18:54 -0300

    Player Animation

---
+++ Assets/Scripts/Game/Player/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    private Vector2 m_inputMovement;
+
+    void Update()
+    {
+        WalkIdleAnimation();
+    }
+
+    private void WalkIdleAnimation()
+    {
+        if (!GameManager.instance.PlayerMovement.CanMove) return;
+
+        m_inputMovement.x = Input.GetAxisRaw("Horizontal");
+        m_inputMovement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Walk", m_inputMovement.magnitude);
+    }
+}
